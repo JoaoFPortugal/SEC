265f2f5466f4b3fceb473b10eef17a78bd6be91a
Nuno <nuno.bras.pinhao@tecnico.ulisboa.pt>
2019-03-28 20:29:24 +0000

    some changes on communication

--- notary/src/main/java/notary/Main.java
+++ notary/src/main/java/notary/Main.java
@@ -3,13 +3,13 @@ package notary;
 import java.io.IOException;
 
 public class Main {
-	
+
 	public static int port = 6066;
 	public static String db_name = "notary.db";
 	public static Database db = null;
-	
+
 	public static void main(String [] args) {
-		
+
 		try {
 			db = new Database(db_name);
 		} catch (IOException e) {
@@ -17,15 +17,15 @@ public class Main {
 			System.exit(0);
 		}
 		db.selectAllUsers();
-		
-        try {
-            Server server = new Server(port);
-            Thread t1 = new Thread(server,"thread1");
-            Thread t2 = new Thread(server,"thread2");
-            t1.start();
-            t2.start();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+
+		try {
+			Server server = new Server(port);
+			Thread producer = new Thread(server,"producer");
+			Thread consumer = new Thread(server,"consumer");
+			producer.start();
+			consumer.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
