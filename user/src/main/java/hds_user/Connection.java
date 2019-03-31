f371e27ade925c03bc4b81751ba19cec4251ad3f
MangaD <davidmanuel1993@hotmail.com>
2019-03-31 17:36:58 +0100

    good format

--- user/src/main/java/hds_user/Connection.java
+++ user/src/main/java/hds_user/Connection.java
@@ -6,42 +6,60 @@ import java.net.Socket;
 import java.io.IOException;
 
 public class Connection {
-	
+
 	private String serverName;
 	private int port;
 	private Socket client;
 	private DataOutputStream out;
 	private DataInputStream in;
-	
+
 	public Connection(String serverName, int port) {
 		this.serverName = serverName;
-		this.port = port;	
+		this.port = port;
 	}
-	
+
 	private void connect() throws IOException {
 		client = new Socket(serverName, port);
 		out = new DataOutputStream(client.getOutputStream());
 		in = new DataInputStream(client.getInputStream());
 	}
-	
+
 	private void disconnect() throws IOException {
 		client.close();
 	}
 
 	/**
-	 * Sends a request to the notary to know if the good is for sale and who owns it.
+	 * Sends a request to the notary to know if the good is for sale and who owns
+	 * it.
 	 */
-	public String getStateOfGood(int good) throws IOException {
+	public Good getStateOfGood(int good) throws IOException {
 		connect();
+		
 		write("getStateOfGood " + Integer.toString(good));
 		String reply = read();
+		String[] tokens = reply.split(" ");
+		if (tokens.length != 4) return null;
+		int id, owner;
+		// test if id is an integer
+		try {
+			id = Integer.parseInt(tokens[0]);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+		// test if owner is an integer
+		try {
+			owner = Integer.parseInt(tokens[2]);
+		} catch (NumberFormatException e) {
+			return null;
+		}
+		Good g = new Good(id, tokens[1], owner, Boolean.valueOf(tokens[3]));
 		disconnect();
-		return reply;
+		return g;
 	}
 
 	/**
-	 * Sends a request to the notary expressing that a good is for sale.
-	 * Fails if user doesn't own it.
+	 * Sends a request to the notary expressing that a good is for sale. Fails if
+	 * user doesn't own it.
 	 */
 	public boolean intentionToSell(int good) throws IOException {
 		connect();
@@ -52,8 +70,8 @@ public class Connection {
 	}
 
 	/**
-	 * Sends a request to the server to change the owner of a good.
-	 * Fails if user doesn't own it.
+	 * Sends a request to the server to change the owner of a good. Fails if user
+	 * doesn't own it.
 	 */
 	public boolean transferGood(int good, int owner) throws IOException {
 		connect();
@@ -62,15 +80,15 @@ public class Connection {
 		disconnect();
 		return success;
 	}
-	
+
 	private String read() throws IOException {
 		return in.readUTF();
 	}
-	
+
 	private void write(String msg) throws IOException {
 		out.writeUTF(msg);
 	}
-	
+
 	public String getAddr() {
 		return client.getRemoteSocketAddress().toString();
 	}
--- user/src/main/java/hds_user/Good.java
+++ user/src/main/java/hds_user/Good.java
@@ -3,11 +3,13 @@ package hds_user;
 public class Good {
 	
 	private int id;
+	private int owner;
     private String name;
     public boolean for_sale;
     
-    public Good(int id, String name, boolean for_sale) {
+    public Good(int id, String name, int owner, boolean for_sale) {
     	this.id = id;
+    	this.owner = owner;
         this.name = name;
         this.for_sale = for_sale;
     }
